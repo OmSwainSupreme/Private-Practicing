c8002ec62872262181c052824ba94294785d1865
Om Prakash Swain <om.swain@outlook.com>
2026-08-25 16:48:59 +0530

    Used static cast

---
+++ Day2/CPP practice.cpp
@@ -0,0 +1,12 @@
+// #include <iostream>
+// #include <ios>
+// int main(){
+//     int a {};
+//     double b {};
+//     std::cout<<"Enter a value for a and b :- ";
+//     std::cin>>a;
+//     std::cin.ignore();
+//     std::cin>>b;
+//     std::cout<<static_cast<double>(a)<<"Here goes b"<<static_cast<int>(b);
+//     return 0;
+// }
